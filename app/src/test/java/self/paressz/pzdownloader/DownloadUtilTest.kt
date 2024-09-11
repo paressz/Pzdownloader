17242560941c9272a1dde00396df1b5d8c03398e
paress <farez0830@gmail.com>
2024-09-11 21:10:02 +0700

    + guessFileType

--- app/src/test/java/self/paressz/pzdownloader/DownloadUtilTest.kt
+++ app/src/test/java/self/paressz/pzdownloader/DownloadUtilTest.kt
@@ -2,14 +2,27 @@ package self.paressz.pzdownloader
 
 import org.junit.Test
 import self.paressz.pzdownloader.util.createFileName
+import self.paressz.pzdownloader.util.guessFileType
 import java.text.SimpleDateFormat
 
 class DownloadUtilTest {
+    val url = "https://pbs.twimg.com/media/GXL5oYUbMAEIMQA?format=jpg&name=4096x4096"
     @Test
     fun `Create File Name Should Return expectedFileName`() {
-        val date = SimpleDateFormat("yyyyMMddHHmmss").format(System.currentTimeMillis())
+        val time = System.currentTimeMillis()
+        val date = SimpleDateFormat("yyyyMMddHHmmss").format(time)
         val platform = "X"
-        val expectedFileName = "PzDl-$platform-$date.mp4"
-        assert(createFileName(platform) == expectedFileName)
+        val type = guessFileType(url)
+        val expectedFileName = "PzDl_${platform}_$date.$type"
+        val actual = createFileName(platform, url, time)
+        println(expectedFileName)
+        println(actual)
+        assert(actual == expectedFileName)
+    }
+    @Test
+    fun `guessFileType() should return jpg`() {
+        val actual = guessFileType(url)
+        val expected = "jpg"
+        assert(actual == expected)
     }
 }
