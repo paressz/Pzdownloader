13bc1bf056bb2e62ca388dc4c51cea35d31f5a56
paress <farez0830@gmail.com>
2024-09-12 20:18:17 +0700

    fix force close on device runnning on Nougat or lower

--- app/src/main/java/self/paressz/pzdownloader/util/DownloadUtil.kt
+++ app/src/main/java/self/paressz/pzdownloader/util/DownloadUtil.kt
@@ -1,6 +1,7 @@
 package self.paressz.pzdownloader.util
 
 import android.content.Context
+import android.os.Build
 import com.ketch.DownloadConfig
 import com.ketch.Ketch
 import com.ketch.NotificationConfig
@@ -11,15 +12,20 @@ fun showDownloadSuccessOrFailed(status: Status, context: Context) {
     if (status == Status.SUCCESS) ToastUtil.showToast(context, context.getString(R.string.download_success))
     else if (status == Status.FAILED) ToastUtil.showToast(context, context.getString(R.string.download_failed))
 }
-
+/**
+ * Configures and returns a [Ketch.Builder]
+ * Notifications are enabled only on Android Oreo (API level 26) or higher.
+ * @return A configured [Ketch.Companion.Builder] instance.
+ */
 fun getKetch(): Ketch.Companion.Builder {
+    val isOreoAndHigher = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
     return Ketch.builder().setDownloadConfig(
             DownloadConfig(
                 connectTimeOutInMs = 15000, readTimeOutInMs = 15000
             )
         ).setNotificationConfig(
             NotificationConfig(
-                enabled = true,
+                enabled = isOreoAndHigher,
                 smallIcon = R.drawable.ic_launcher_foreground,
                 showSpeed = true,
                 showSize = true
